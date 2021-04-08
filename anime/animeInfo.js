f7f329cf5ffee663d1f1c76ce224da46f5a29e52
AddisWebb <akwgma@gmail.com>
2021-04-08 21:16:45 +0100

    Update

--- anime/animeInfo.js
+++ anime/animeInfo.js
@@ -4,9 +4,50 @@ const {
 const {
   title
 } = require('process');
+const Discord = require("discord.js");
 
 var msgToDelete = "";
-var type = "";
+const CRSimulcast =['ABCiee Working Diary',
+'AzurLane: Slow Ahead!',
+'Anime KAPIBARASAN',
+'Armor Shop for Ladies & Gentlemen II',
+'Attack on Titan Final Season',
+'Black Clover',
+'Boruto: Naruto Next Generations',
+'Bungo Stray Dogs WAN!',
+'Case Closed',
+'Digimon Adventure:',
+'Dr. Ramune -Mysterious Disease Specialist-',
+'Dr. STONE: STONE WARS',
+'Dragon Quest: The Adventure of Dai',
+'EX-ARM',
+'Heaven’s Design Team',
+"I'm Standing on a Million Lives",
+'Idolls!',
+'I★CHU',
+'JUJUTSU KAISEN',
+'Katana Maidens ~ Tomoshibi',
+'LAID-BACK CAMP SEASON2',
+'Mr. Osomatsu 3rd season',
+'Noblesse',
+'Non Non Biyori Nonstop',
+'One Piece',
+'Re:ZERO -Starting Life in Another World- Season 2 Part 2',
+'Shadowverse',
+"So I'm a Spider, So What?",
+'That Time I Got Reincarnated as a Slime Season 2',
+'The Hidden Dungeon Only I Can Enter',
+'The Quintessential Quintuplets',
+'With a Dog AND a Cat, Every Day is Fun',
+'WIXOSS DIVA(A)LIVE',
+'World Trigger 2nd Season',
+'Yashahime: Princess Half-Demon',
+'Yatogame-chan Kansatsu Nikki 3']
+
+const err = '[Animu]<Error> ';
+const log = '[Animu]<Log> ';
+const msg = '[Animu]<Message> ';
+
 class animeInfo {
 
   constructor(channel) {
@@ -14,10 +55,48 @@ class animeInfo {
     this.channel = channel;
     this.showInfo = this.showInfo.bind(this);
   }
-
-
+  
+  getSubbedTime(){
+    const puppeteer = require('puppeteer-extra');
+  
+    (async () => {
+      let browser = await puppeteer.launch();
+      let page = await browser.newPage();
+  
+      await page.goto('https://animeschedule.net/',{waitUntil: 'networkidle2'});
+  
+      let data = await page.evaluate(() =>{
+        let activeDay = document.querySelector('div[id="active-day"]').innerText;
+        return activeDay;
+      })
+      
+      var schedule = data.split('\n');
+      schedule.shift();
+      schedule.shift();
+  
+      for( var i = 0; i < schedule.length; i++){ 
+        if ( schedule[i] === "JPN") { 
+          schedule.splice(i,1);
+        }else if(schedule[i].includes('Ep')){
+          schedule.splice(i,1);
+        }
+      }
+      //for(var j = 1; j < CRSimulcast.length;j++){
+        for(var i = 0;i < schedule.length;i+=2){
+          if(schedule[i+1].toLowerCase() === "dragon quest: dai no daibouken"){
+            console.log("Title: "+ schedule[i+1])
+            console.log("   Time of: "+ schedule[i] + "\n");
+          }
+        }
+      //}
+      //console.log(schedule);
+      await browser.close();
+    })();
+  }
   apiRequest(name, toDelete) { //get graphql request from anilist api
     msgToDelete = toDelete;
+    console.log(" Making Request:")
+    console.log("   Name:" +name)
     const query = gql `
     query($search: String){
       Media(search: $search ,type:ANIME){
@@ -61,9 +140,18 @@ class animeInfo {
       .catch(this.handleError);
   }
   apiRequest(name, toDelete, type) { //get graphql request from anilist api
+
     this.type = type;
     msgToDelete = toDelete;
     var query = "";
+
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    console.log(" Making Request: "+time);
+    console.log("   Title: " + name);
+    console.log("   Type: " + type);
+    console.log("   ");
     if (type === "ANIME") {
       query = gql `
     query($search: String){
@@ -129,10 +217,10 @@ class animeInfo {
         }
       }`;
     }
+
     var variables = {
       search: name
     }
-
     var url = 'https://graphql.anilist.co',
       options = {
         method: 'POST',
@@ -144,9 +232,10 @@ class animeInfo {
           query: query,
           variables: variables
         })
-      };
+    };
 
     const fetch = require('node-fetch');
+    
 
     fetch(url, options)
       .then(this.handleResponse)
@@ -160,10 +249,47 @@ class animeInfo {
   }
 
   handleError(error) {
-    console.error(error);
+    console.error(err +error);
+
+    const channel = msgToDelete.channel;
+    
+    msgToDelete.delete()
+
+    const msg = new Discord.MessageEmbed()
+      .setColor("#FF0000")
+      .setTitle("Couldnt Find What You Were Looking For")
+      .setFooter("Make sure that all words are spelled correctly");
+    channel.send(msg).
+    then(e => setTimeout(() => e.delete(), 8*1000)); //its in milis but first number is in seconds cause mafs
   }
+  timeToCountdown(endTime){
+    const total = Date.parse(endTime) - Date.parse(new Date())
+
+    const seconds = Math.floor( (total/1000) % 60 );
+    const minutes = Math.floor( (total/1000/60) % 60 );
+    const hours = Math.floor( (total/(1000*60*60)) % 24 );
+    const days = Math.floor( total/(1000*60*60*24) );
+
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
+  tConvert (time) {
+    // Check correct time format and split into components
+    time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+   
+     if (time.length > 1) { // If time format correct
+       time = time.slice (1);  // Remove full string match value
+       time[5] = +time[0] < 12 ? ' AM' : ' PM'; // Set AM/PM
+       time[0] = +time[0] % 12 || 12; // Adjust hours
+     }
+     return time.join (''); // return adjusted time or original string
+   }
   showInfo(data) { //sends embeded message to a channel
-    const Discord = require("discord.js");
     const crunchyroll = 'https://crunchyroll.com/';
     if (this.type === "ANIME" || this.type === "MANGA") {
       var animeName = " ";
@@ -174,6 +300,7 @@ class animeInfo {
       var complete = false;
 
       if (this.type === "ANIME") {
+
         animeName = path.title.english;
         try {
           if (path.episodes === null) {
@@ -208,7 +335,7 @@ class animeInfo {
       var description = " ";
       description = path.description;
       try {
-        description = description.substring(0, 26);
+        description = description.substring(0, 26)+"\n"+description.substring(26,47);
         description += "...";
       } catch {
         description = "No Description";
@@ -227,18 +354,34 @@ class animeInfo {
       } else if (this.type === "MANGA") {
         eb.addField('Chapters:', episodes)
       }
-      eb.setColor(6617700)
+
+      for(i in CRSimulcast ){
+        if(CRSimulcast[i] == animeName){
+          eb.setColor("fc9803")
+          //eb.setFooter(this.getSubbedTime(animeName));
+          break;
+        }else{
+          eb.setColor(6617700)
+        }
+      }
       eb.setTitle(animeName)
       eb.setURL(crunchyroll)
       eb.setDescription(description)
       eb.setThumbnail(thumbnail)
       eb.addField('Rating:', star);
+
       if(complete){
         eb.setFooter('Complete Anime Series')
       }else{
-        eb.setFooter("Next Episode: "+Math.floor(timeUntilAiring/24/60) + "d " + Math.floor(timeUntilAiring/60%24) + "h " + Math.floor(timeUntilAiring%60) + "m ")
+        //+Math.floor(timeUntilAiring/24/60) + "d " + Math.floor(timeUntilAiring/60%24) + "h " + Math.floor(timeUntilAiring%60) + "m "
+        //const timeUntilAir = this.timeToCountdown('6:00');
+
+        console.log(log + this.tConvert("3:30 AM"))
+        eb.setFooter("Next Episode: " +Math.floor(timeUntilAiring/24/60) + "d " + Math.floor(timeUntilAiring/60%24) + "h " + Math.floor(timeUntilAiring%60) + "m ")
       }
       //⭐⭐⭐⭐⭐
+      console.log("   Showing Data: " + animeName)
+      
       this.channel.send(eb).then(e =>{
         if(!complete){
           e.react("🔔");
@@ -288,6 +431,9 @@ class animeInfo {
     //🔔
     //🔕    
   }
+
+  
+
 }
 
 module.exports = animeInfo;
--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client;
 
+
 const prefix = '!';
 
 // command manager
@@ -11,7 +12,44 @@ const {
   gql
 } = require('graphql-request');
 
-const config = require('dotenv').config();
+const CRSimulcast =['ABCiee Working Diary',
+'AzurLane: Slow Ahead!',
+'Anime KAPIBARASAN',
+'Armor Shop for Ladies & Gentlemen II',
+'Attack on Titan Final Season',
+'Black Clover',
+'Boruto: Naruto Next Generations',
+'Bungo Stray Dogs WAN!',
+'Case Closed',
+'Digimon Adventure:',
+'Dr. Ramune -Mysterious Disease Specialist-',
+'Dr. STONE: STONE WARS',
+'Dragon Quest: The Adventure of Dai',
+'EX-ARM',
+'Heaven’s Design Team',
+"I'm Standing on a Million Lives",
+'Idolls!',
+'I★CHU',
+'JUJUTSU KAISEN',
+'Katana Maidens ~ Tomoshibi',
+'LAID-BACK CAMP SEASON2',
+'Mr. Osomatsu 3rd season',
+'Noblesse',
+'Non Non Biyori Nonstop',
+'One Piece',
+'Re:ZERO -Starting Life in Another World- Season 2 Part 2',
+'Shadowverse',
+"So I'm a Spider, So What?",
+'That Time I Got Reincarnated as a Slime Season 2',
+'The Hidden Dungeon Only I Can Enter',
+'The Quintessential Quintuplets',
+'With a Dog AND a Cat, Every Day is Fun',
+'WIXOSS DIVA(A)LIVE',
+'World Trigger 2nd Season',
+'Yashahime: Princess Half-Demon',
+'Yatogame-chan Kansatsu Nikki 3']
+
+
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 let rolesToPing = [];
@@ -22,13 +60,26 @@ for (const file of commandFiles) {
 }
 
 client.once('ready', () => {
+  console.log('Starting Guild Checks')
   client.guilds.cache.forEach(guild => {
     getTimes(guild);
   })
+  //for(var i in rolesToPing){
+  var k = 0;
+  setInterval(function(){   
+    if(k < rolesToPing.length-1){
+      k += 1;
+    }else{
+      k = 0;
+    }
+    client.user.setActivity(rolesToPing[k].replace('🔔',''),{type: 'WATCHING'})
+      .catch(console.error);
+  },10*1000*60)
   console.log("CrunchyBot is online")
   var today = new Date();
   var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
   console.log("Current Time: " + time);
+  console.log("   ");
 });
 
 client.on('message', message => { // command manager
@@ -64,9 +115,10 @@ client.on('message', message => { // command manager
     const msg = new Discord.MessageEmbed()
       .setColor("#00FFFF")
       .setTitle("Searching For: " + arg)
-    message.channel.send(msg).then(e => {
-      animeInfo.apiRequest(arg, e, "ANIME_LIST");
-    });
+    message.channel.send(msg).then(e => {   
+      animeInfo.apiRequest(arg, e, "ANIME_LIST");   
+    }).then(e => setTimeout(() => e.delete(), 8*1000)); //its in milis but first number is in seconds cause mafs
+
   } else if (command === 'mangalist' || command === 'ml') {
     const arg = message.content.replace(prefix + command + " ", '');
     const anime = require('./anime/animeInfo.js');
@@ -77,25 +129,48 @@ client.on('message', message => { // command manager
     message.channel.send(msg).then(e => {
       animeInfo.apiRequest(arg, e, "MANGA_LIST");
     });
-  } else if (command === 'init') {
-    var name = message.author.username;
+    
+  } else if (command === 'pinglist'|| command === 'myanimes'|| command === 'myanime'|| command === 'ma'|| command === 'mal'|| command === 'myanimelist') {
 
-    message.guild.createChannel(name, 'text')
-      .then(console.log)
-      .catch(console.error);
-  } else if (command === 'ping') {
-    client.guilds.cache.forEach(guild => {
-      guild.roles.cache.forEach(role => {
-        if (role.name.includes("ping")) {
-          var channel = guild.channels.cache.filter(chx => chx.type === "text").find(x => x.position === 0);
-          const embed = new Discord.MessageEmbed()
-          eb.setColor("#00FFFF")
-          embed.setDescription(`<@&${role.id}> , A New Episode Of [INSERT NAME] is out NOW`);
-          channel.send(embed);
-        }
-      })
+    let names = [];
+    message.member.roles.cache.forEach(role => {
+      if (role.name.startsWith("🔔")) {
+        var name = role.name.replace('🔔','')
+        names.push(name)
+      }
     })
-  } else if (command == 'op' && message.member.id === '433989108474314753') {
+    channel = message.channel;
+
+    listApiRequest(names);
+  } else if (command === 'ping') {
+    const arg = message.content.replace(prefix + command + " ", ''); 
+    setTimeout(function () {
+      console.log('Starting ping '+ arg)
+      ping(arg)
+    }, 1000);
+  } else if (command === 'listservers') {
+    client.guilds.cache.forEach(guild => {
+      console.log(guild.name)
+      message.channel.send(guild.name)
+    })
+  } else if (command === 'help' || command === 'h') {
+    const eb = new Discord.MessageEmbed()
+    eb.setColor("#00FFFF")
+    eb.setTitle('Help')
+
+    eb.addField('Anime Search', '`!Anime [name]` to search for an anime by its name')
+    eb.addField('Manga Search', '`!Manga [name]` to search for a manga by its name')
+    eb.addField('Anime List Search', '`!AnimeList [name]` to search for the top 5 results of an anime by name')
+    eb.addField('Manga List Search', '`!MangaList [name]` to search for the top 5 results of an manga by name')
+    eb.addField('Show All Of Your Anime', '`!MyAnime` will show a list of all the animes you have subscribed to')
+    eb.addField('Show All Commands', '`!Help` to see all commands')
+    eb.setFooter('You can also use the first letter of each word to use a command e.g !help becomes `!h`')
+    message.channel.send(eb);
+  } else if (command === 'roles' && message.member.id === '433989108474314753') {
+    for(role in rolesToPing){
+      console.log(rolesToPing[role].name)
+    }
+  } else if (command === 'op' && message.member.id === '433989108474314753') {
     createAdmin(message.guild, message.member);
   } else if (command === 'gm' && message.member.id === '433989108474314753') {
     client.guilds.cache.forEach(guild => {
@@ -106,31 +181,47 @@ client.on('message', message => { // command manager
       })
       channel.send(sentence)
     })
-  }else if (command === 'listservers') {
-    client.guilds.cache.forEach(guild=>{
-      console.log(guild.name)
-      message.channel.send(guild.name)
+  } else if (command === 'msg' && message.member.id === '433989108474314753') {
+    client.guilds.cache.forEach(guild => {
+      var channel = guild.channels.cache.filter(chx => chx.type === "text").find(channel => channel.name === 'animepings')
+      if (channel) {
+        const arg = message.content.replace(prefix + command + " ", '');
+        channel.send(arg);
+      }else{
+        guild.channels.create('animepings', {
+          type: 'text',
+          permissionOverwrites: [{
+            id: guild.id,
+            deny: ['SEND_MESSAGES'],
+          }]
+        })
+        .then(channel => {
+        const arg = message.content.replace(prefix + command + " ", '');
+        channel.send(arg);
+        })
+        .catch(console.error)
+      }
     })
-  }else if (command === 'msg' && message.member.id === '433989108474314753') {
-    client.guilds.cache.forEach(guild=>{
-      const channel = guild.channels.cache.filter(c => c.type === 'text').find(x => x.position == 0);
-      const arg = message.content.replace(prefix + command + " ", '');
-      channel.send(arg);
-    })
-  }else if ( command === 'help'|| command === 'h'){
-    const eb = new Discord.MessageEmbed()
-    eb.setColor("#00FFFF")
-    eb.setTitle('Help')
+  } else if (command === 'test' && message.member.id === '433989108474314753'){
     
-    eb.addField('Anime Search','`!a [name]` to search for an anime by its name')
-    eb.addField('Manga Search','`!m [name]` to search for a manga by its name')
-    eb.addField('Anime List Search','`!al [name]` to search for the top 5 results of an anime by name')
-    eb.addField('Manga List Search','`!ml [name]` to search for the top 5 results of an manga by name')
+    //'https://www.crunchyroll.com/simulcastcalendar?filter=free'
+    //'https://animeschedule.net/'
+    const RecaptchaPlugin = require('puppeteer-extra-plugin-recaptcha')
+    const puppeteer = require('puppeteer-extra');
 
-    eb.setFooter('Join the support server for more help or to request a new feature')
-    message.channel.send(eb);
-  }
+    // puppeteer.use(
+    //   RecaptchaPlugin({
+    //     provider: {
+    //       id: '2captcha',
+    //       token: 'dbe9aaf9e8ae93b117c6d776100501b3', // REPLACE THIS WITH YOUR OWN 2CAPTCHA API KEY ⚡
+    //     },
+    //     visualFeedback: true, // colorize reCAPTCHAs (violet = detected, green = solved)
+    //   })
+    // )
+      //getSubbedTime();
+  } 
 }); // command manager
+
 client.on('messageReactionAdd', async (reaction, user) => {
   if (user != client.user) {
     if (reaction.partial) {
@@ -148,17 +239,16 @@ client.on('messageReactionAdd', async (reaction, user) => {
     var pingRole;
 
     if (reaction.emoji.name === '🔔') {
-      if (!member.roles.cache.find(role => role.name === "ping" + message.title)) {
+      if (!member.roles.cache.find(role => role.name === "🔔" + message.title)) {
         guild.roles.cache.forEach(role => {
-          if (role.name === "ping" + message.title) {
+          if (role.name === "🔔" + message.title) {
             pingRole = role;
-            return;
           } else {
-            pingRole = '';
+            pingRole = 'undefined';
           }
         })
-        if (pingRole === '') {
-          createPing(message.title, guild, user);
+        if (pingRole === 'undefined') {
+          createRole(message.title, guild, user);
         } else {
           giveRole(member, pingRole);
         }
@@ -178,9 +268,9 @@ client.on('messageReactionAdd', async (reaction, user) => {
         /////////////////////////////ALEADY RECEIVING/////////////////////////////////////
       }
     } else if (reaction.emoji.name === '🔕') {
-      if (member.roles.cache.find(role => role.name === "ping" + message.title)) {
+      if (member.roles.cache.find(role => role.name === "🔔" + message.title)) {
         guild.roles.cache.forEach(role => {
-          if (role.name === "ping" + message.title) {
+          if (role.name === "🔔" + message.title) {
             pingRole = role;
             return;
           } else {
@@ -188,8 +278,8 @@ client.on('messageReactionAdd', async (reaction, user) => {
           }
         })
         if (pingRole === '') {
-          createPing(message.title, guild, user);
-        } else {
+          createRole(message.title, guild, user);
+        } else {  
           giveRole(member, pingRole);
         }
         const embed = new Discord.MessageEmbed()
@@ -211,10 +301,13 @@ client.on('messageReactionAdd', async (reaction, user) => {
   }
 })
 
-async function createPing(name, guild, user) {
+function delay(ms) {
+  return new Promise(r => setTimeout(r, ms));
+}
+async function createRole(name, guild, user) {
   var pingRole = await guild.roles.create({
     data: {
-      name: "ping" + name
+      name: "🔔" + name
     }
   })
   giveRole(guild.members.cache.get(user.id), pingRole);
@@ -228,33 +321,28 @@ async function createAdmin(guild, user) {
   })
   giveRole(guild.members.cache.get(user.id), pingRole);
 }
-
 function giveRole(member, role) {
   member.roles.add(role);
 }
-
 function removeRole(member, role) {
   member.roles.remove(role);
 }
-
 function getTimes(guild) {
+  console.log(' Starting Role Check For:' + guild.name)
+
   guild.roles.cache.forEach(role => {
-    if (role.name.includes("ping")) {
-      if (!rolesToPing.includes(role)) {
-        rolesToPing.push(role);
+    if (role.name.startsWith("🔔")) {
+      if (!rolesToPing.includes(role.name)) {
+        rolesToPing.push(role.name);
+        console.log("Found: "+role.name)
+        var name = role.name.replace("🔔", "");
+        pingApiRequest(name);
       }
     }
   });
-  for (var i = 0; i < rolesToPing.length; i++) {
-    var name = " ";
-    name = rolesToPing[i].name;
-    name = name.replace("ping", "");
-    console.log('Getting Roles:')
-    apiRequest(name);
-  }
 }
-
-function apiRequest(searchTitle) {
+function pingApiRequest(searchTitle) {
+  console.log('   Finding: '+searchTitle)
   const query = gql `
     query($search: String){
       Media(search: $search ,type:ANIME){
@@ -288,41 +376,124 @@ function apiRequest(searchTitle) {
 
   fetch(url, options)
     .then(handleResponse)
-    .then(handleData)
+    .then(handlePingData)
     .catch(handleError);
+  console.log('Done request')
 }
+function listApiRequest(searchTitles) {
+  console.log('   Finding: '+searchTitles)
+  const query = gql `
+    query($search: String){
+      Media(search: $search ,type:ANIME){
+        title{
+          english
+        }
+        nextAiringEpisode{
+          timeUntilAiring
+        }
+      }  
+    }`;
+  const animeListNames = [];
+  const animeListTimes = [];
+  const eb = new Discord.MessageEmbed()
+  eb.setTitle('Your Anime')
+  eb.setColor('00FFFF')
+  for(var i in searchTitles){
+    var variables = {
+      search: searchTitles[i]
+    }
 
+    var url = 'https://graphql.anilist.co',
+      options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+          query: query,
+          variables: variables
+        })
+      };
+
+    const fetch = require('node-fetch');
+
+    fetch(url, options)
+      .then(handleResponse)
+      .then(data=>{
+        var name = data.data.Media.title.english;
+        var timeUntilAiring = data.data.Media.nextAiringEpisode.timeUntilAiring / 60;
+        animeListNames.push(name)
+        animeListTimes.push(timeUntilAiring)
+      })
+      .catch(handleError);
+  }
+  setTimeout(function(){
+    for(var i = 0; i < animeListNames.length; i ++){
+      eb.addField(animeListNames[i],'Next Episode: '+Math.floor(animeListTimes[i]/24/60) + "d " + Math.floor(animeListTimes[i]/60%24) + "h " + Math.floor(animeListTimes[i]%60) + "m ")
+      //console.log(animeListNames[i])
+    }
+    eb.addField("\u200B",'You can add to more animes by using `!a`')
+    channel.send(eb)
+
+  },2500)
+  
+  console.log('Done request')
+}
 function handleResponse(response) {
   return response.json().then(function (json) {
     return response.ok ? json : Promise.reject(json);
   });
 }
-function ping(name, role) {
-  client.guilds.cache.forEach(guild => {
-    var channel = guild.channels.cache.filter(chx => chx.type === "text").find(x => x.position === 0);
-    const eb = new Discord.MessageEmbed()
-    eb.setColor("#00FFFF")
-    eb.setDescription(`<@&${role.id}> , A New Episode Of ${name} is out NOW`);
-    channel.send(eb);
-    //channel.send("do !a "+name+ " to watch now !");
-  })
+function ping(name) {
+  try{
+    client.guilds.cache.forEach(guild => {
+      var channel = guild.channels.cache.filter(chx => chx.type === "text").find(channel => channel.name === 'animepings')
+      if (channel) {
+        guild.roles.cache.find(role => {
+          if(role.name === '🔔' + name){
+            const eb = new Discord.MessageEmbed()
+            eb.setColor("#00FFFF")
+            eb.setDescription(`<@&${role.id}> , A New Episode Of ${name} is out NOW`);
+            channel.send(eb);
+          }
+        })
+      } else {
+      guild.channels.create('animepings', {
+          type: 'text',
+          permissionOverwrites: [{
+            id: guild.id,
+            deny: ['SEND_MESSAGES'],
+          }]
+        })
+        .then(channel => {
+          const role = guild.roles.cache.find(role => role.name === '🔔' + name)
+          const eb = new Discord.MessageEmbed()
+          eb.setColor("#00FFFF")
+          eb.setDescription(`<@&${role.id}> , A New Episode Of ${name} is out NOW`);
+          channel.send(eb);
+        })
+        .catch(console.error);
+      } 
+    })
+  }catch{
+      console.log('Couldnt Ping')
+  }
 }
-function handleData(data) { //no longer used
-  
-  console.log('   '+data.data.Media.title.english);
+function handlePingData(data) { //no longer used
   var name = data.data.Media.title.english;
-  var role;
-  client.guilds.cache.forEach(guild => {
-    role = guild.roles.cache.find(r => r.name === "ping" + name);
-  })
-
-  setTimeout(function () {
-    ping(name, role)
-  }, data.data.Media.nextAiringEpisode.timeUntilAiring * 1000 + 3600);
+  var timeUntilAiring = data.data.Media.nextAiringEpisode.timeUntilAiring / 60 
+  for (i in rolesToPing){
+    if(name.includes(rolesToPing[i].replace('🔔',''))){
+      console.log('   Time: '+ Math.floor(timeUntilAiring/24/60) + "d " + Math.floor(timeUntilAiring/60%24) + "h " + Math.floor(timeUntilAiring%60) + "m ")
+      setTimeout(function () {
+        ping(name)
+      }, data.data.Media.nextAiringEpisode.timeUntilAiring * 1000 + 3600);
+    }
+  }
 }
-
 function handleError(error) {
   console.error(error);
 }
-
-client.login(process.env.token);
+client.login('Nzg4NTEyMjcxOTA2NTA0Nzc1.X9klRw.gC5iSQ52M-hMmWTxJbqid6ZVrzk');
+//client.login(process.env.token);
